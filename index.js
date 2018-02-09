d0c1875ceab7633b44ec7593154a7e8a60777ef1
zach.zeid <zach.zeid@gmail.com>
2018-02-09 14:31:11 -0500

    Updated HTML/CSS, Added index.js for frontend actions.

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  console.log("Document ready");
+  $('#add-vpc-col').click(function(){
+    console.log('Adding VPC');
+  })
+  $('#remove-vpc').click(function(){
+    console.log('Removing VPC');
+  })
+})
