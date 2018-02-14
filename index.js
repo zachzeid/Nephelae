142a34e9f456d200cd60aaf68c7c28bb0f1bbc3f
zach.zeid <zach.zeid@gmail.com>
2018-02-14 10:04:15 -0500

    Changed js lib to ip-address and tested subnetting with ip-address module.

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 $(document).ready(function() {
+
   console.log("Document ready");
+  $('#vpc-cidr').change((event) => {
+
+    let vpcCidr = $(event.currentTarget).val();
+    var cidr = new Address4(vpcCidr);
+  });
+
+  $('#subnet1').change((event) => {
+    vpcCidr = new Address4("192.168.1.0/24")
+    var address = new Address4($(event.currentTarget).val());
+    console.log(address.isInSubnet(vpcCidr));
+    if (address.isInSubnet(vpcCidr)) {
+      console.log(address.correctForm() + " is in range");
+    } else {
+      flash(address.correctForm() + " is not in range.");
+    }
+    });
+
   $('#add-vpc-col').click(function(){
     console.log('Adding VPC');
   })
   $('#remove-vpc').click(function(){
     console.log('Removing VPC');
   })
+
 })
