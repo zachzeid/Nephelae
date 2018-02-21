a12e6599c8790d0d93b3d20a52182e1b687fbf60
zach.zeid <zach.zeid@gmail.com>
2018-02-21 17:10:40 -0500

    Trying my hand out at JS for loops and inserting child elements.

--- index.js
+++ index.js
@@ -1,28 +1,51 @@
-$(document).ready(function() {
+$(document).ready(() => {
+  $.validator.addMethod('address4', (val) => {
+    const ip = new Address4(val);
+    return ip.isValid();
+  }, 'Invalid IP Address');
 
-  console.log("Document ready");
+  $('form').validate();
+  console.log('Document ready');
   $('#vpc-cidr').change((event) => {
-
-    let vpcCidr = $(event.currentTarget).val();
-    var cidr = new Address4(vpcCidr);
+    const vpcCidr = $(event.currentTarget).val();
+    console.log(vpcCidr);
+    // var cidr = new Address4(vpcCidr);
   });
 
+  // $('#subnet-count').change((event) => {
+  //
+  // });
+  // let step;
+  // for (step = 0; step < 5; step += 1) {
+  //   console.log('walking east one step', step);
+  // };
+  $('#subnet-count').change((event) => {
+    const $selector = $('#subnet-main');
+    const subnetCount = $(event.currentTarget).val();
+    for (let count = 0; count <= subnetCount; count += 1) {
+      console.log(`subnet-${count}`);
+      $selector.append($(`<label for="subnet-${count}">Subnet CIDR</label>`));
+      $selector.append($(`< input class = "form-control"
+                    type = "text"
+                    id = "subnet-${count}" >`));
+    }
+  });
   $('#subnet1').change((event) => {
-    vpcCidr = new Address4("192.168.1.0/24")
-    var address = new Address4($(event.currentTarget).val());
+    const vpcCidr = new Address4('192.168.1.0/27');
+    const address = new Address4($(event.currentTarget).val());
     console.log(address.isInSubnet(vpcCidr));
     if (address.isInSubnet(vpcCidr)) {
-      console.log(address.correctForm() + " is in range");
+      console.log(address.correctForm());
     } else {
-      flash(address.correctForm() + " is not in range.");
+      alert(address.correctForm());
     }
-    });
+  });
 
-  $('#add-vpc-col').click(function(){
+  $('#add-vpc-col').click(() => {
     console.log('Adding VPC');
-  })
-  $('#remove-vpc').click(function(){
-    console.log('Removing VPC');
-  })
+  });
 
-})
+  $('#remove-vpc').click(() => {
+    console.log('Removing VPC');
+  });
+});
